dd6792a854c6251ef4ea1869740fae0ef9d7b6f3
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2026-09-12 07:20:27 +0100

    Make webview extend iframe's component instead of duplicating its logic

--- packages/webview/addon/components/webview.js
+++ packages/webview/addon/components/webview.js
@@ -1,45 +1,9 @@
-import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
+import Iframe from '@ascua/iframe/components/iframe';
 
-export default class extends Component {
-
-	@tracked frame;
-
-	get src() {
-		return this.args.src || 'about:blank';
-	}
-
-	get zoom() {
-		return this.args.zoom || 1;
-	}
-
-	get width() {
-		return this.args.width || '100%';
-	}
-
-	get height() {
-		return this.args.height || '100%';
-	}
+export default class extends Iframe {
 
 	get partition() {
 		return Math.random().toString(36).substr(2, 10);
-	}
-
-	get scale() {
-		return Math.min(1, this.frame / this.zoom);
-	}
-
-	get perct() {
-		return Math.max(100, (1 / this.scale) * 100) + '%';
-	}
-
-	get trans() {
-		return `scale(${this.scale})`;
-	}
-
-	@action didCreate(element) {
-		this.frame = element.offsetWidth;
 	}
 
 }
